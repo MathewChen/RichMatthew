179efa8ae539382b61c861f4e455c433449b5756
清香白莲 <1243327527@qq.com>
2020-11-23 11:41:45 +0800

    fitst commit

---
+++ RichMatthew/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for RichMatthew project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RichMatthew.settings')
+
+application = get_wsgi_application()
